f9d2027d330edaef3336c510a05a1258aa4e04e0
Morgan Mack <mackleemorgan@gmail.com>
2021-04-20 17:15:57 -0400

    BE: Model association and seed data complete

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,128 @@
-'use strict'
+'use strict';
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User },
+} = require('../server/db');
+
+const products = [
+  {
+    name: 'Phillip',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Grey',
+    fit: 'Classic',
+    price: 700,
+    size: 43,
+    description: 'An iconic suit.',
+  },
+  {
+    name: 'Phillip',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Navy Blue',
+    fit: 'Classic',
+    price: 700,
+    size: 41,
+    description: 'An iconic suit.',
+  },
+  {
+    name: 'George',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Royal Blue',
+    fit: 'Slim',
+    price: 650,
+    size: 40,
+    description: 'A new fit on an iconic suit.',
+  },
+  {
+    name: 'Harry',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Black',
+    fit: 'Slim',
+    price: 650,
+    size: 42,
+    description: 'The ultimate party suit.',
+  },
+  {
+    name: 'James',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Khaki',
+    fit: 'Classic',
+    price: 600,
+    size: 44,
+    description: 'A suit for the summer.',
+  },
+  {
+    name: 'Henry',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Charcoal',
+    fit: 'Classic',
+    price: 600,
+    size: 45,
+    description: 'Go for smokey and smoldering.',
+  },
+  {
+    name: 'Charles',
+    category: 'Suit',
+    imageUrl:
+      'https://cdn.entertainmentdaily.com/2020/07/22132320/prince-philip-1-scaled.jpg',
+    color: 'Olive',
+    fit: 'Classic',
+    price: 600,
+    size: 40,
+    description: 'Money on your mind? Dress like it.',
+  },
+];
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log('db synced!');
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({
+      username: 'cody',
+      password: '123',
+      email: 'codytheyak@aol.com',
+    }),
+    User.create({
+      username: 'murphy',
+      password: '123',
+      email: 'murphyslaw@aol.com',
+    }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  // Creating Products
+  await Promise.all(
+    products.map((product) => {
+      return Product.create(product);
+    })
+  );
+
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded ${products.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -32,16 +131,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log('seeding...');
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log('closing db connection');
+    await db.close();
+    console.log('db connection closed');
   }
 }
 
@@ -51,8 +150,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,22 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
+const User = require('./models/User');
+const Order = require('./models/Order');
+const Product = require('./models/Product');
 
-//associations could go here!
+//associations could go here
+User.hasMany(Order);
+Order.belongsTo(User);
+Order.belongsToMany(Product);
+Product.belongsToMany(Order);
 
 module.exports = {
   db,
   models: {
     User,
+    Order,
+    Product,
   },
-}
+};
