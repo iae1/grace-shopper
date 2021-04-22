e4897f8eec706988bfe67cdbe5620bd635b49419
Juan Auli <juanseauly@gmail.com>
2021-04-22 18:11:57 -0400

    BE: Builds GET and PUT routes for logged in cart

--- server/api/users.js
+++ server/api/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { requireToken, isAdmin } = require('./gatekeeping');
 const {
-  models: { User },
+  models: { User, Product, Order },
 } = require('../db');
 module.exports = router;
 
@@ -13,3 +13,46 @@ router.get('/', requireToken, isAdmin, async (req, res, next) => {
     next(err);
   }
 });
+
+//GET pending order (i.e., cart) route for logged in user
+router.get('/:id/cart', requireToken, async (req,res,next) => {
+  try {
+    if (req.user && req.user.id.toString() === req.params.id) {
+      const cart = await Order.findOne({
+        include: Product,
+        where: {
+          userId: req.params.id,
+          order_status: 'pending'
+        }
+      });
+      if (cart) return res.status(200).send(cart)
+      return res.status(200).send("This user doesn't have a cart")
+    }
+    res.status(200).send("You don't have permission to see this user's cart")
+  } catch (err) {
+    next(err);
+  }
+})
+//PUT pending order (i.e., cart) route to checkout for logged in user
+router.put('/:id/cart', requireToken, async (req,res,next) => {
+  try {
+    // checking out
+    if (req.user && req.user.id.toString() === req.params.id) {
+      const cart = await Order.findOne({
+        include: Product,
+        where: {
+          userId: req.params.id,
+          order_status: 'pending'
+        }
+      });
+      if (cart) {
+        const updatedCart = await cart.update({order_status: 'completed'})
+        return res.status(200).send(updatedCart);
+      }
+      return res.send("This user doesn't have a cart")
+    }
+    res.send("You don't have permission to modify this user's cart")
+  } catch (err) {
+    next(err);
+  }
+})
