9f28866e6a653bbb2b9115c1d94ce39f49dfeb5f
Morgan Mack <mackleemorgan@gmail.com>
2021-04-27 18:30:24 -0400

    FE: Submitting order now clears cart in local storage

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -5,7 +5,7 @@ import { checkoutLoggedUser, checkoutGuestUser } from '../store/cart';
 // global
 const token = window.localStorage.getItem('token');
 
-export class Checkout extends React.Component{
+export class Checkout extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -14,145 +14,152 @@ export class Checkout extends React.Component{
       email: '',
       nameError: '',
       emailError: '',
-      addressError: ''
-    }
+      addressError: '',
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleStateChange = this.handleStateChange.bind(this);
-    this.validate = this.validate.bind(this)
+    this.validate = this.validate.bind(this);
   }
 
   handleStateChange(e) {
     this.setState({
       [e.target.name]: e.target.value,
-    })
+    });
   }
 
   validate() {
-    let nameError = ''
-    let emailError = ''
-    let addressError = ''
+    let nameError = '';
+    let emailError = '';
+    let addressError = '';
 
     if (!this.state.name) {
-      nameError = "Order must include name"
+      nameError = 'Order must include name';
     }
 
     if (!this.state.address) {
-      addressError = "Order must include address"
+      addressError = 'Order must include address';
     }
 
     if (!this.state.email) {
-      emailError = "Order must include email"
+      emailError = 'Order must include email';
     }
 
     if (nameError || addressError || emailError) {
-      this.setState({nameError, addressError, emailError})
-      return false
+      this.setState({ nameError, addressError, emailError });
+      return false;
     }
 
-    return true
+    return true;
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    const isValid = this.validate()
+    const isValid = this.validate();
     if (isValid) {
       const address = this.state.address;
       const email = this.state.email;
-      if (this.props.auth.id) { // if the user is logged in
-        this.props.checkoutLoggedUser({address, token})
-        console.log('cart--->', this.props.cart)
+      if (this.props.auth.id) {
+        // if the user is logged in
+        this.props.checkoutLoggedUser({ address, token });
+        console.log('cart--->', this.props.cart);
         // this.props.history.push('/orderconfirmation')
       } else {
         //need to pull cart off of local storage... waiting on morgan
         const guestCart = JSON.parse(localStorage.getItem('cart'));
-        const cartItems = []
-        for (let i=0; i<guestCart.length; i++) {
+        const cartItems = [];
+        for (let i = 0; i < guestCart.length; i++) {
           cartItems.push({
             quantity: parseInt(guestCart[i].quantity, 10),
             fit: guestCart[i].fit,
             size: guestCart[i].size,
             length: guestCart[i].length,
-            productId: guestCart[i].id
-          })
+            productId: guestCart[i].id,
+          });
         }
-        console.log("cart items--->", cartItems)
-        this.props.checkoutGuestUser({email, address, cartItems})
-        console.log("guest cart--->", this.props.cart)
+        console.log('cart items--->', cartItems);
+        this.props.checkoutGuestUser({ email, address, cartItems });
+        console.log('guest cart--->', this.props.cart);
       }
-      this.props.history.push('/orderconfirmation')
+      this.props.history.push('/orderconfirmation');
+      localStorage.clear();
     }
   }
 
   render() {
-    let totalPrice = 0
+    let totalPrice = 0;
     let guestCart;
     if (this.props.auth.id) {
-      const products = this.props.cart.cart.products
-      console.log('products--->', products)
+      const products = this.props.cart.cart.products;
+      console.log('products--->', products);
       for (let i = 0; i < products.length; i++) {
-        totalPrice += (products[i].price * products[i].order_details.quantity)
+        totalPrice += products[i].price * products[i].order_details.quantity;
       }
     } else {
       guestCart = JSON.parse(localStorage.getItem('cart'));
-      for (let i =0; i<guestCart.length; i++) {
-        totalPrice += guestCart[i].price
+      for (let i = 0; i < guestCart.length; i++) {
+        totalPrice += guestCart[i].price;
       }
     }
     // console.log('totalPrice', totalPrice)
     return (
       <React.Fragment>
         <div id='total-price'>
-          Total Price: ${
-          totalPrice > 0 
-          ? totalPrice
-          : null
-          }
+          Total Price: ${totalPrice > 0 ? totalPrice : null}
         </div>
-        <form id="checkout-cart-form" onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Your Name:</label>
-          <input name="name" value={this.state.name} onChange={this.handleStateChange} placeholder="required" />
-          {
-            this.state.nameError
-            ? <div style={{ color: "red "}}>{this.state.nameError}</div>
-            : null
-          }
-          <label htmlFor="address">Your Billing/Shipping Address:</label>
-          <input name="address" value={this.state.address} onChange={this.handleStateChange} placeholder="required" />
-          {
-            this.state.addressError
-            ? <div style={{ color: "red "}}>{this.state.addressError}</div>
-            : null
-          }
-          <label htmlFor="email">Your Email:</label>
-          <input name="email" value={this.state.email} onChange={this.handleStateChange} placeholder="required" />
-          {
-            this.state.emailError
-            ? <div style={{ color: "red "}}>{this.state.emailError}</div>
-            : null
-          }
-          <button type="submit">Submit Order</button>
+        <form id='checkout-cart-form' onSubmit={this.handleSubmit}>
+          <label htmlFor='name'>Your Name:</label>
+          <input
+            name='name'
+            value={this.state.name}
+            onChange={this.handleStateChange}
+            placeholder='required'
+          />
+          {this.state.nameError ? (
+            <div style={{ color: 'red ' }}>{this.state.nameError}</div>
+          ) : null}
+          <label htmlFor='address'>Your Billing/Shipping Address:</label>
+          <input
+            name='address'
+            value={this.state.address}
+            onChange={this.handleStateChange}
+            placeholder='required'
+          />
+          {this.state.addressError ? (
+            <div style={{ color: 'red ' }}>{this.state.addressError}</div>
+          ) : null}
+          <label htmlFor='email'>Your Email:</label>
+          <input
+            name='email'
+            value={this.state.email}
+            onChange={this.handleStateChange}
+            placeholder='required'
+          />
+          {this.state.emailError ? (
+            <div style={{ color: 'red ' }}>{this.state.emailError}</div>
+          ) : null}
+          <button type='submit'>Submit Order</button>
         </form>
       </React.Fragment>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     cart: state.cart,
-    auth: state.auth
+    auth: state.auth,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     checkoutLoggedUser: (info) => {
-      dispatch(checkoutLoggedUser(info))
+      dispatch(checkoutLoggedUser(info));
     },
     checkoutGuestUser: (info) => {
-      dispatch(checkoutGuestUser(info))
-    }
-  }
-}
+      dispatch(checkoutGuestUser(info));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
